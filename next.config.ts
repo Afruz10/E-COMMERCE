275c64df97c936de957d6012f5520e2fdedadec1
Afruz10 <checkapps38@gmail.com>
2026-07-24 10:09:17 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/googleda03cfbbb2a469da.html', // 👈 Yahan apni Google HTML file ka exact naam dalo
+        destination: '/api/google-verify',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
